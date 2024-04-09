a81412375733549436cd9e52ab26b3794e0cacbe
mgomezandreu <mario-gomez-andreu@outlook.com>
2024-04-09 12:56:40 +0200

    implemented functor models

--- optim/solvers.py
+++ optim/solvers.py
@@ -9,7 +9,7 @@ import pandas as pd
 logging.basicConfig(level =logging.INFO)
 
 from gsplat_traj_optim.splines.bsplines import  spline_eval
-from gsplat_traj_optim.convolution.gaussian_robot import curve_robot_obstacle_convolution
+from gsplat_traj_optim.convolution.gaussian_robot import create_curve_robot_obstacle_convolution_functor
 
 def create_solver(num_control_points, n_gaussians, dim_control_points=3, num_samples=30, sym_type="SX"):
     """ Return casadi solver object and upper and lower bounds for the optimization problem
@@ -71,9 +71,9 @@ def create_solver(num_control_points, n_gaussians, dim_control_points=3, num_sam
 
     accel_cost = cas.sum1(cas.sum2(ddcurve**2))
 
+    convolution_functor = create_curve_robot_obstacle_convolution_functor(num_samples, n_gaussians, dim_control_points)
+    obstacle_cost = convolution_functor(curve, robot_cov, obstacle_means, obstacle_covs)
 
-
-    obstacle_cost = curve_robot_obstacle_convolution(curve, robot_cov, obstacle_means, obstacle_covs)
     cost = length_cost +  1000000 * obstacle_cost + 0.1 *accel_cost
 
     # define optimization solver
@@ -84,45 +84,5 @@ def create_solver(num_control_points, n_gaussians, dim_control_points=3, num_sam
 
     return solver, lbg, ubg
 
-
-
-
-"""
-class SolverManager:
-    def __init__(self, solver_dir):
-        self.solver_dir = solver_dir
-        
-        # load record file if exists
-        if os.path.exists(solver_dir + "record.csv"):
-            self.record = pd.read_csv(solver_dir + "record.csv")
-        else:
-            self.record = pd.DataFrame(columns = ["id","num_gaussians", "num_control_points", "num_samples", "dim_control_points"])
-
-    def get_solver(self, id):
-        pass
-
-    def get_solver(self, num_gaussians, num_control_points, num_samples, dim_control_points):
-        # check if solver already exists
-        if self.record[(self.record["num_gaussians"] == num_gaussians) \ 
-            & (self.record["num_control_points"] == num_control_points) \
-            & (self.record["num_samples"] == num_samples) \
-            & (self.record["dim_control_points"] == dim_control_points)].shape[0] > 0:
-
-            ids = self.record[(self.record["num_gaussians"] == num_gaussians) \
-                & (self.record["num_control_points"] == num_control_points) \
-                & (self.record["num_samples"] == num_samples) \
-                & (self.record["dim_control_points"] == dim_control_points)]["id"].values[0]
-            return self.get_solver(ids[0])
-        else:
-            return self.create_solver(num_gaussians, num_control_points, num_samples, dim_control_points)
-  
-    def create_solver(self, num_gaussians, num_control_points, num_samples, dim_control_points):
-        pass
-
-
-    def save_solver(self, solver, lbg, ub, lbg, ubgg, num_gaussians, num_control_points, num_samples, dim_control_points):
-        pass
-
-"""      
 
 
