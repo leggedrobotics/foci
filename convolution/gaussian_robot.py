aa889c797920aa2cc9da8d44be969a47dd627c3f
mgomezandreu <mario-gomez-andreu@outlook.com>
2024-04-11 13:51:33 +0200

    removed reshaping and added omp option

--- convolution/gaussian_robot.py
+++ convolution/gaussian_robot.py
@@ -10,54 +10,75 @@ def create_normal_pdf_functor(dim):
     x = SYM_TYPE.sym("x", dim, 1)
     mu = SYM_TYPE.sym("mu", dim, 1)
     cov_det = SYM_TYPE.sym("cov_det", 1, 1)
-    cov_inv = SYM_TYPE.sym("cov_inv", dim, dim)
+    cov_inv = SYM_TYPE.sym("cov_inv", dim , dim )
 
-    pdf = cas.exp(-0.5 * (x-mu).T@ cov_inv @ (x-mu)) / (cas.sqrt(2 * cas.pi) * cov_det)
+
+    pdf = cas.exp(-0.5 * (x-mu).T@ cov_inv@ (x-mu)) / (cas.sqrt(2 * cas.pi) * cov_det)
+
+    print("cov_inv shape",cov_inv.shape)
 
     return cas.Function("normal_pdf", [x, mu, cov_det, cov_inv], [pdf])
 
-def create_robot_obstacle_convolution_functor(num_obstacles, dim):
+def create_matrix_inverse_functor(dim):
     SYM_TYPE = cas.SX
-    pdf_functor = create_normal_pdf_functor(dim)
+
+    cov = SYM_TYPE.sym("cov", dim, dim)
+    cov_inv = cas.pinv(cov)
+
+    return cas.Function("matrix_inverse", [cov], [cov_inv])
+
+def create_matrix_det_functor(dim):
+    SYM_TYPE = cas.SX
+
+    cov = SYM_TYPE.sym("cov", dim, dim)
+    cov_det = cas.det(cov)
+
+    return cas.Function("matrix_det", [cov], [cov_det])
+
+
+def create_robot_obstacle_convolution_functor(num_obstacles, dim, parallelization = "openmp"):
+    SYM_TYPE = cas.SX
+    pdf_functor_map = create_normal_pdf_functor(dim).map(num_obstacles, parallelization)
+    inverse_functor_map = create_matrix_inverse_functor(dim).map(num_obstacles, parallelization)
+    det_functor_map = create_matrix_det_functor(dim).map(num_obstacles, parallelization)
     
     robot_mean = SYM_TYPE.sym("robot_mean", dim, 1)
-    robot_cov = SYM_TYPE.sym("robot_cov", dim * dim)
+    robot_cov = SYM_TYPE.sym("robot_cov", dim, dim)
     obstacle_means = SYM_TYPE.sym("obstacle_means", dim, num_obstacles)
-    obstacle_covs = SYM_TYPE.sym("obstacle_covs", dim * dim, num_obstacles)
+    obstacle_covs = SYM_TYPE.sym("obstacle_covs", dim, dim *  num_obstacles)
 
-    obstacle_covs_ = []
-    for i in range(obstacle_covs.shape[1]): 
-        obstacle_covs_.append(cas.reshape(obstacle_covs[:,i], 3,3))
-    robot_cov_ = cas.reshape(robot_cov, 3,3)
 
-    covs_sum = [robot_cov_ + obstacle_cov for obstacle_cov in obstacle_covs_]
-    covs_sum_det = [cas.det(cov) for cov in covs_sum]
-    covs_sum_inv = [cas.pinv(cov) for cov in covs_sum]
+    covs_sum = obstacle_covs + robot_cov 
+    covs_sum_det = det_functor_map(covs_sum)
+    covs_sum_inv = inverse_functor_map(covs_sum)
+
+    print("covs_sum_inv shape",covs_sum_inv.shape) 
+    print("covs_sum_det shape",covs_sum_det.shape)  
     
-    conv = 0
+    
+    pdf_evals = pdf_functor_map(robot_mean, obstacle_means, covs_sum_det, covs_sum_inv)
+    norm_conv = cas.sum2(pdf_evals) / num_obstacles
 
-    for i in range(num_obstacles):
-        conv += pdf_functor(robot_mean, obstacle_means[:,i], covs_sum_det[i], covs_sum_inv[i])
-
-    norm_conv = conv / num_obstacles
     return cas.Function("robot_obstacle_convolution", [robot_mean, robot_cov, obstacle_means, obstacle_covs], [norm_conv])
  
 
-def create_curve_robot_obstacle_convolution_functor(num_samples, num_obstacles, dim):
+def create_curve_robot_obstacle_convolution_functor(num_samples, num_obstacles, dim, parallelization = "openmp"):
     SYM_TYPE = cas.MX
 
-    convolution_functor = create_robot_obstacle_convolution_functor(num_obstacles, dim)
-
-    curve = SYM_TYPE.sym("curve", num_samples, dim)
-    robot_cov = SYM_TYPE.sym("robot_cov", dim * dim)
+    convolution_functor = create_robot_obstacle_convolution_functor(num_obstacles, dim, parallelization = parallelization)
+    
+    curve = SYM_TYPE.sym("curve", dim, num_samples)
+    robot_cov = SYM_TYPE.sym("robot_cov", dim , dim)
     obstacle_means = SYM_TYPE.sym("obstacle_means", dim, num_obstacles)
-    obstacle_covs = SYM_TYPE.sym("obstacle_covs", dim * dim, num_obstacles)
-    
-    conv = 0
-    for i in range(num_samples):
-        logging.info(f"Computing convolution for point {i}")
-        point = curve[i,:]
-        conv += convolution_functor(point, robot_cov, obstacle_means, obstacle_covs)
+    obstacle_covs = SYM_TYPE.sym("obstacle_covs", dim, dim *  num_obstacles)
 
-    return cas.Function("curve_robot_obstacle_convolution", [curve, robot_cov, obstacle_means, obstacle_covs], [conv])
-    
+
+#    convolution_functor_with_set_parameters = cas.Function("robot_obstacle_convolution", [point], [convolution_functor(point, robot_cov, obstacle_means, obstacle_covs)])
+
+    convolution_map = convolution_functor.map(num_samples, parallelization)
+    out_map = convolution_map(curve, robot_cov, obstacle_means, obstacle_covs)
+    print("out_map shape",out_map.shape)
+
+    out = cas.sum2(out_map)/ num_samples # Does that change solver speed?
+    print("outshape",out.shape)   
+    return cas.Function("robot_obstacle_convolution", [curve, robot_cov, obstacle_means, obstacle_covs], [out])
