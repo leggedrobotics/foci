fixed bad jacobian transformation

convolution/gaussian_robot.py
@@ -13,13 +13,11 @@ def create_normal_pdf_functor(dim):
     cov_inv = SYM_TYPE.sym("cov_inv", dim , dim )
 
 
-    pdf = cas.exp(-0.5 * (x-mu).T@ cov_inv@ (x-mu)) / (cas.sqrt(2 * cas.pi) * cov_det)
+    pdf = cas.exp(-0.5 * (x-mu).T@ cov_inv@ (x-mu)) * 1000/cov_det 
 
-    print("cov_inv shape",cov_inv.shape)
 
     return cas.Function("normal_pdf", [x, mu, cov_det, cov_inv], [pdf])
 
-    print("pdf shape",pdf.shape)
 
 def create_matrix_inverse_functor(dim):
     SYM_TYPE = cas.SX
@@ -54,8 +52,6 @@ def create_robot_obstacle_convolution_functor(num_obstacles, dim, parallelizatio
     covs_sum_det = det_functor_map(covs_sum)
     covs_sum_inv = inverse_functor_map(covs_sum)
 
-    print("covs_sum_inv shape",covs_sum_inv.shape) 
-    print("covs_sum_det shape",covs_sum_det.shape)  
     
     
     pdf_evals = pdf_functor_map(robot_mean, obstacle_means, covs_sum_det, covs_sum_inv)
@@ -79,10 +75,8 @@ def create_curve_robot_obstacle_convolution_functor(num_samples, num_obstacles, 
 
     convolution_map = convolution_functor.map(num_samples, parallelization)
     out_map = convolution_map(curve, robot_cov, obstacle_means, obstacle_covs)
-    print("out_map shape",out_map.shape)
 
     out = cas.sum2(out_map)/ num_samples # Does that change solver speed?
-    print("outshape",out.shape)   
     return cas.Function("robot_obstacle_convolution", [curve, robot_cov, obstacle_means, obstacle_covs], [out])
 
 
@@ -103,19 +97,15 @@ if __name__ == "__main__":
     obstacle_means = np.ones((dim, num_obstacles))
     points = np.zeros((dim, num_points))
 
-    print("Here")
 
     conv = create_curve_robot_obstacle_convolution_functor(num_points, num_obstacles, dim)
 
     hello = conv(points.T, robot_cov, obstacle_means, covs)
-    print(hello)
-    print(hello)
 
     y = MX.sym("curve", dim, num_points)
     jac = Function("jac", [y], [jacobian(conv(y), y)])
 
     out = jac(points)
 
-    print(out.shape)
 
 

convolution/gaussian_robot_warp.py
@@ -23,7 +23,6 @@ class ConvolutionFunctorWarp(Callback):
         assert len(obstacle_means) > 0, "The number of obstacles should be greater than 0"
         assert covs_inv.shape[1:] == (dim,dim), "The shape of the covs_inv should be (dim,dim)"
 
-        print("Number of obstacles: ", len(obstacle_means)) 
         
         self.dim = dim
         self.num_points = num_points
@@ -51,7 +50,7 @@ class ConvolutionFunctorWarp(Callback):
             m,n = wp.tid() # m is the point index, n is the obstacle index
 
             diff = points[m] - obstacle_means[n]
-            normal =  wp.exp(-0.5 * wp.dot(diff, covs_inv[n] @ diff))
+            normal =  wp.exp(-0.5 * wp.dot(diff, covs_inv[n] @ diff)) * 1000.0/covs_det[n]
             wp.atomic_add(intermediate, n, normal)
             
         self.f = f
@@ -72,20 +71,17 @@ class ConvolutionFunctorWarp(Callback):
 
     # Evaluate numerically
     def eval(self, arg):
-
-
         points= np.array(arg[0])
         self.points_gpu = wp.from_numpy(points, dtype=wp.vec3)
         self.intermediate.zero_()
         self.out.zero_()
-
 
         wp.launch(kernel = self.f,
                 dim = (self.num_points, self.num_obstacles),
                 inputs = [self.points_gpu, self.obstacle_means, self.covs_det, self.covs_inv,self.intermediate])
 
         wp.utils.array_sum(self.intermediate, out = self.out)
-        out =  self.out.numpy()[0]
+        out =  self.out.numpy()[0] / (self.num_obstacles * self.num_points)
         return [out]
 
 
@@ -117,7 +113,7 @@ class ConvolutionFunctorWarp(Callback):
                     m,n = wp.tid() # m is the point index, n is the obstacle index
 
                     diff = points[m] - obstacle_means[n]
-                    normal =  wp.exp(-0.5 * wp.dot(diff, covs_inv[n] @ diff)) 
+                    normal =  wp.exp(-0.5 * wp.dot(diff, covs_inv[n] @ diff)) * 1000.0/covs_det[n]
 
                     sub_gradient = -normal * covs_inv[n] @ diff
                     intermediate[m,n] = sub_gradient
@@ -154,8 +150,10 @@ class ConvolutionFunctorWarp(Callback):
                         dim = (self.num_points, self.num_obstacles),
                         inputs = [self.points_gpu, self.obstacle_means, self.covs_det, self.covs_inv,self.intermediate])
                 
-                wp.utils.array_sum(self.intermediate, out = self.out, axis = 1)
-                out = self.out.numpy().flatten().reshape(1, self.num_points * self.dim)
+                # wp.utils.array_sum(self.intermediate, out = self.out, axis = 1)
+                out = wp.utils.array_sum(self.intermediate, axis = 1)
+                out = out.numpy().transpose().reshape(1, self.num_points * self.dim)                
+                out = out / (self.num_obstacles * self.num_points)
                 return [out]
 
         self.jac_callback = JacFun(self.dim, self.num_points, self.obstacle_means, self.covs_det, self.covs_inv)
@@ -234,7 +232,6 @@ if __name__ == "__main__":
     control_points_opt = np.array(res['x']).reshape(num_points, dim)
     opt_curve = spline_eval(control_points_opt, 200)
     
-    print(grad(control_points_opt))
 
     print(opt_curve)
 